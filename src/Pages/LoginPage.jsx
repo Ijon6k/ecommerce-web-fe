2bb1257dec9d47e04d9ccb28d4f2a501f227f723
nexblu <farras.pramudita@gmail.com>
2024-05-01 21:42:49 +0700

    feat: add api login

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,7 +1,35 @@
 import { Link } from "react-router-dom";
 import WebLogo from "../Components/WebLogo.jsx";
+import { useState } from "react";
+import { jwtDecode } from 'jwt-decode';
+import { useNavigate } from "react-router-dom";
+import Cookies from 'js-cookie';
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const apiLogin = async (email, password) => {
+    const response = await fetch(`http://127.0.0.1:5000/e-commerce/v1/login/${email}/${password}`);
+    const data = await response.json();
+    if (data['status_code'] === 200) {
+      const decodedToken = jwtDecode(data['result']['token']);
+      return decodedToken
+    } else {
+      return false
+    }
+  }
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const result = apiLogin(email, password)
+    if (result) {
+      Cookies.set('access_token', result);
+      navigate('/')
+    }
+  }
   return (
     <div className="flex h-screen max-h-screen w-screen flex-col gap-20 bg-white p-14 lg:flex-row">
       {/* Bagian untuk form */}
@@ -9,20 +37,24 @@ const LoginPage = () => {
         <h1 className="mb-10 text-4xl font-semibold">Welcome Back 👋🏻</h1>
         <WebLogo></WebLogo>
         <p className="my-10 font-semibold">Please enter your details</p>
-        <form className=" sm:w-3/4 lg:w-4/5">
+        <form className=" sm:w-3/4 lg:w-4/5" onSubmit={handleLogin}>
           {/* Menggunakan komponen InputForm untuk email */}
           <input
             type="text"
             name="email"
             placeholder="Email"
             className="mb-2 w-full rounded-full border border-black p-3 pl-8  text-lg text-slate-700 outline-none"
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)}
           />
           {/* Menggunakan komponen InputForm untuk password */}
           <input
-            type="text"
+            type="password"
             name="password"
             placeholder="Password"
             className="mb-2 w-full rounded-full border border-black p-3 pl-8  text-lg text-slate-700 outline-none"
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)}
           />
           {/* Tombol Login */}
           <div className="mb-10 mt-5 flex items-center justify-between">
